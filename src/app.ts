2caace0649f2b264b89f5ca8ecd093efcf22c2a4
ditobayu <ditobayuzs1@gmail.com>
2024-05-07 20:48:49 +0700

    fix: delete app comment

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,9 @@ import Router from "../routes/index";
 
 const app = express();
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
-// app.get("/", (req: Request, res: Response) => {
-//   res.json({ message: "Success!" });
-// });
-
-// app.get("/:id", (req: Request, res: Response) => {
-//   const id: string = req.params.id;
-
-//   res.json({ message: `Success with ID: ${id}` });
-// });
-
 app.use(Router);
 
 app.listen(8000, () => {
